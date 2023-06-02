121bddebf5b12efa5bbd20e17f27e0840cb9f9ad
SaiIndra2003 <suryasaiindra@gmail.com>
2023-06-02 23:15:07 +0530

    cors error handling and register in server side

--- server.js
+++ server.js
@@ -21,6 +21,48 @@ mongoose
   .then(() => console.log("Connected to Atlas-MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
+// Handle cors error
+
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.set("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.post("/register", async (req, res, next) => {
+  try {
+    const password = await bcrypt.hash(req.body.password, 6);
+
+    const user = new User({
+      _id: mongoose.Types.ObjectId(),
+      name: req.body.name,
+      email: req.body.email,
+      contact: req.body.contact,
+      username: req.body.username,
+      password: password,
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      message: "User created succesfully...",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Registration failed",
+      error: err,
+    });
+  }
+});
+
 app.listen(3001, () => {
   console.log("Server Running on port 3001");
 });
