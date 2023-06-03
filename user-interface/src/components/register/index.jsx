8cd8e2bdd4fd1281121fe18185311f471ba04fb4
SaiIndra2003 <suryasaiindra@gmail.com>
2023-06-03 22:22:59 +0530

    mere changes

--- user-interface/src/components/register/index.jsx
+++ user-interface/src/components/register/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-import { registerUser } from "../api";
+import { registerUser } from "../api/api";
 
 import "./styles.scss";
 
