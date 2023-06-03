6e2f6618d91447429f08410b546886c66e38203c
SaiIndra2003 <suryasaiindra@gmail.com>
2023-06-03 12:51:15 +0530

    basic ui setup

--- user-interface/src/App.js
+++ user-interface/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Register from "./components/register/index";
+import LoginForm from "./components/login/index";
+import Profile from "./components/profile/index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/** Header */}
+      <div className="App__content-wrapper">
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+      {/** Footer */}
     </div>
   );
 }
